3a5b9bfd0c928acfc7474a4f7a7be6f1663bfad7
Alymbekov <maxim.makarov.1997@mail.ru>
2019-05-18 15:32:33 +0600

    d

--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello world !!!")
-    
+    return render(request, 'index.html', {})
+
 # Create your views here.
