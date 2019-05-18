447bc31f4bbc1f394fd52f3d5caffcebc99b1faa
Aijamal <askhatovaaijamalgmail.com>
2019-05-18 20:21:25 +0600

    modified views

--- booking/views.py
+++ booking/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from booking.models import Field, Image
-from django.views.generic import TemplateView
+from booking.models import Field
+
 
 def booking(request):
     fields = Field.objects.all()
-    image = Image.objects.all()
     return render(request, 'booking/booking.html', {
         'fields': fields,
-        'images': image,
     })
 
-class IndexView(TemplateView):
-    template_name = 'website/main.html'
 
 
 
-
+    
 # Create your views here.
