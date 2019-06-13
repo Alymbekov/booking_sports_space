c56dfc083f2fdcfa30a03545691b261c87c80138
Aizat K <abhkst912@gmail.com>
2019-06-13 18:46:48 +0600

    changed nav

--- booking/static/js/burger_menu.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $(".burger-menu").click(function() {
-      $(".stick").toggleClass(function () {
-        return $(this).is('.open, .close') ? 'open close' : 'open';
-      });
-    });
-  });
